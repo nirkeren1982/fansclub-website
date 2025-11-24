14e3ff36c107d9b467bff0a3ffd96f8602a8bed8
nirkeren1982 <nirkeren.21@gmail.com>
2025-11-24 16:27:45 +0200

    Match activity tags section width with other content sections
    - Changed container max-width from max-w-3xl to max-w-5xl
    - Activity tags section now has same width as Main content and Why Subscribe sections on desktop

--- src/pages/CreatorProfile.tsx
+++ src/pages/CreatorProfile.tsx
@@ -262,7 +262,7 @@ const CreatorProfile = () => {
         {/* Services Section - Activity Tags */}
         {activities.length > 0 && (
           <section id="content" className="w-full py-4 bg-background">
-            <div className="container px-8 md:px-12 lg:px-16 max-w-3xl mx-auto">
+            <div className="container px-8 md:px-12 lg:px-16 max-w-5xl mx-auto">
               <ActivityTagsSection activities={activities} creatorName={creator.display_name || creator.username} username={creator.username} />
             </div>
           </section>
